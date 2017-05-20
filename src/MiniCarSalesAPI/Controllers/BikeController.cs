3bad6c34987663f3ebc99cab872b3a7b12055768
Sandeep Sekhon <sandeep.sekhon@hotmail.com>
2017-05-20 17:53:05 +1000

    Added controllers for posting Car and Bike
    Signed-off-by: Sandeep Sekhon <sandeep.sekhon@hotmail.com>

---
+++ src/MiniCarSalesAPI/Controllers/BikeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
+using MiniCarSales.Data.InMemoryRepository;
+using MiniCarsales.Data.Models;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MiniCarSalesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [EnableCors("AllowAllOrigins")]
+    public class BikeController : Controller
+    {
+        private readonly IInMemoryData<Vehicle> _vehicleProvider;
+        public BikeController(IInMemoryData<Vehicle> vehicleProvider)
+        {
+            _vehicleProvider = vehicleProvider;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]Bike bike)
+        {
+            _vehicleProvider.Add(bike);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id:guid}")]
+        public void Put(Guid id, [FromBody]Bike bike)
+        {
+            _vehicleProvider.Edit(id, bike);
+        }
+
+    }
+}
---
+++ src/MiniCarSalesAPI/Controllers/CarController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
+using MiniCarSales.Data.InMemoryRepository;
+using MiniCarsales.Data.Models;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MiniCarSalesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [EnableCors("AllowAllOrigins")]
+    public class CarController : Controller
+    {
+        private readonly IInMemoryData<Vehicle> _vehicleProvider;
+
+        public CarController(IInMemoryData<Vehicle> vehicleProvider)
+        {
+            _vehicleProvider = vehicleProvider;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]Car car)
+        {
+            _vehicleProvider.Add(car);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id:guid}")]
+        public void Put(Guid id, [FromBody]Car car)
+        {
+            _vehicleProvider.Edit(id, car);
+        }
+    }
+}
