15346df8398f1e1675abb3814b2f3c19ab7a2398
Sandeep Sekhon <sandeep.sekhon@hotmail.com>
2017-05-21 10:02:00 +1000

    Switching to new version of .NET core as the old version was bug prone and had issues with detection of unit tests. See refernce: https://github.com/dotnet/cli/issues/4046 Changing to target framework: netstandard1.6. Previous version: 1.0.0-preview2-003131

---
+++ MiniCarSales.API.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MiniCarSales.API.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true, " Dummy test failed");
+        }
+    }
+}
